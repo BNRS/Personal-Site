63d8d9422b72dee01db4d1456bc404ce8a9e1d6c
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-03-11 22:38:01 -0500

    setting up framework for my personal site

--- Personal-Site/app.js
+++ Personal-Site/app.js
@@ -1,2 +1,29 @@
 "use strict";
 
+//this adds a clock to the page//
+function clock() {
+
+    var fullDate = new Date();
+    var hour = fullDate.getHours();
+    var min = fullDate.getMinutes();
+    var sec = fullDate.getSeconds();
+
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+    document.getElementById('hours').innerHTML = hour;
+    document.getElementById('minutes').innerHTML = ": " + min;
+    document.getElementById('seconds').innerHTML = ": " + sec;
+
+    setInterval(clock, 100);
+
+}
+
+clock();
