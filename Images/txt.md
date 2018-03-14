db2fe9964b7ad727489d3501163bb77c4176ec7a
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-03-14 13:27:33 -0500

    top of page almost complete

---
+++ Images/txt.md
@@ -0,0 +1,6 @@
+Photo by Karsten Würth (@inf1783) on Unsplash - mountains
+Dmitri Popov - minato
+Ryoji Iwata - crossing
+
+Photo by Benjamin Hung on Unsplash - tokyo-night
+Photo by Evgeny Lazarenko on Unsplash - cherry blossom
--- Personal-Site/app.js
+++ Personal-Site/app.js
@@ -1,29 +1,33 @@
 "use strict";
 
-//this adds a clock to the page//
-function clock() {
+    function clock() {
 
-    var fullDate = new Date();
-    var hour = fullDate.getHours();
-    var min = fullDate.getMinutes();
-    var sec = fullDate.getSeconds();
+        var fullDate = new Date();
+        var day = fullDate.getDay();
+        var date = fullDate.getDate();
+        var month = fullDate.getMonth();
+        var hour = fullDate.getHours();
+        var min = fullDate.getMinutes();
+        var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    if (hour < 10) {
-        hour = "0" + hour;
+        if (hour > 12) {
+            hour += -12;
+            min += " PM";
+        } else if (hour < 12) {
+            hour = "0" + hour;
+            min += " AM";
+        }
+        if (min < 10) {
+            min = "0" + min;
+        }
+
+        document.getElementById('day').innerHTML = daylist[day];
+        document.getElementById('month').innerHTML = monthList[month] + " " + date + ",";
+        document.getElementById('hours').innerHTML = hour;
+        document.getElementById('minutes').innerHTML = ": " + min;
+
+        setInterval(clock, 100);
+
     }
-    if (min < 10) {
-        min = "0" + min;
-    }
-    if (sec < 10) {
-        sec = "0" + sec;
-    }
 
-    document.getElementById('hours').innerHTML = hour;
-    document.getElementById('minutes').innerHTML = ": " + min;
-    document.getElementById('seconds').innerHTML = ": " + sec;
-
-    setInterval(clock, 100);
-
-}
-
-clock();
