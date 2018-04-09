b7d336a8fd525ee95f8f04ca662a0be889081a7a
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-04-08 23:48:20 -0500

    work on personal site

--- Personal-Site/app.js
+++ Personal-Site/app.js
@@ -1,5 +1,68 @@
 "use strict";
 
+$('.gh').hide();
+
+// Show main heading
+$('.github-anchor').hover(function() {
+    $('.gh').toggle();
+});
+
+$('.C').hide();
+
+// Show main heading
+$('.portfolio-anchor').hover(function() {
+    $('.C').toggle();
+});
+
+
+
+
+
+
+
+
+
+// function hideIcons() {
+//     $('.github-icon').hide();
+//     $('.linkedin-icon').hide();
+//     $('.portfolio-icon').hide();
+//     $('.about-me-icon').hide();
+// }
+// hideIcons();
+//
+// function showIcons() {
+//     $('.github-icon').hover().fadeIn();
+//     $('.linkedin-icon').hide();
+//     $('.portfolio-icon').hide();
+//     $('.about-me-icon').hide();
+// }
+
+
+
+
+
+
+
+// function loadGif() {
+//     $('.container-gif').html("<img src='./img/paddle.gif' class='load'>");
+// }
+// loadGif();
+
+// hide form until called in getMovies function
+// $('.add-movie-form').hide();
+// $('.search-box').hide();
+// $('.movie-table').hide();
+// $('.container-gif').hide();
+
+
+// this function shows the forms upon page load when called
+// function formLoader() {
+//     $('.add-movie-form').show();
+//     $('.search-box').show();
+//     $('.movie-table').show();
+//     $('.container-loader').hide();
+// }
+
     function clock() {
 
         var fullDate = new Date();
